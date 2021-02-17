d98899817154bc3737ae560f39c2ad4c313f0d1b
Sarunas Janickas <sarunas.janickas@gmail.com>
2021-02-17 09:41:14 +0200

    basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
+import { RoomComponent } from './components/room/room.component';
+import { RoomListComponent } from './components/room-list/room-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelComponent,
+    HotelListComponent,
+    RoomComponent,
+    RoomListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
