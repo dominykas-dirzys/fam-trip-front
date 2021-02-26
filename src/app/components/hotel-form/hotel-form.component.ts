bdd588896d5c423382983090d99ec5a543b1c9c0
Sarunas Janickas <sarunas.janickas@gmail.com>
2021-02-26 10:54:14 +0200

    reference-data.service - but data undefined in hotel-form.component

--- src/app/components/hotel-form/hotel-form.component.ts
+++ src/app/components/hotel-form/hotel-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, Input, OnInit, SimpleChanges} from '@angular/core';
 import {FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {City, Hotel, CityGroup, ReferenceData} from '../../types/types';
