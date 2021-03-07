1936345be74a56dabfb4350cce22b4bac043de58
Sarunas Janickas <sarunas.janickas@gmail.com>
2021-03-07 09:18:45 +0200

    working on apiService

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -3,32 +3,19 @@ import {HttpClient} from '@angular/common/http';
 import {TOKEN_KEY} from '../common/constants';
 import {Observable} from 'rxjs';
 import {City} from '../types/types';
+import {ApiService} from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
 
-  private citiesUrl: string;
+  private static readonly URL: '/api/cities';
 
-  constructor(private http: HttpClient) {
-    this.citiesUrl = 'http://localhost:8080/api/cities';
+  constructor(private api: ApiService) {
   }
 
   public findAll(): Observable<City[]> {
-    return this.http.get<City[]>(this.citiesUrl, this.getRequestOptions());
-  }
-
-  private getRequestOptions() {
-    const token = sessionStorage.getItem(TOKEN_KEY);
-
-    const headers: { [key: string]: string } = {
-      'Content-Type': 'application/json',
-      Authorization: token ? `Bearer ${token}` : ''
-    };
-
-    return {
-      headers
-    };
+    return this.api.get<City[]>(CityService.URL);
   }
 }
--- src/app/services/reference-data.service.ts
+++ src/app/services/reference-data.service.ts
@@ -11,93 +11,86 @@ import {TOKEN_KEY} from "../common/constants";
 })
 export class ReferenceDataService {
 
-  private referenceDataUrl: string;
-
-  constructor(private http: HttpClient) {
-    this.referenceDataUrl = 'http://localhost:8080/api/reference_data';
-  }
-
-  public findAll(): Observable<ReferenceData[]> {
-    return this.http.get<ReferenceData[]>(this.referenceDataUrl, this.getRequestOptions());
-  }
-
-  private getRequestOptions() {
-    const token = sessionStorage.getItem(TOKEN_KEY);
-
-    const headers: { [key: string]: string } = {
-      'Content-Type': 'application/json',
-      Authorization: token ? `Bearer ${token}` : ''
-    };
-
-    return {
-      headers
-    };
-  }
-
-  // private static readonly URL = '/api/reference_data';
-  // private cuisineTypes = [];
-  // private foodQualities = [];
-  // private hotelLabels = [];
-  // private hotelRatings = [];
-  // private recommendedTos = [];
-  // private roomConditions = [];
-  // private roomTypes = [];
-  // private sizes = [];
+  // private referenceDataUrl: string;
   //
-  // constructor(private api: ApiService) {
+  // constructor(private http: HttpClient) {
+  //   this.referenceDataUrl = 'http://localhost:8080/api/reference_data';
   // }
   //
-  // public findAll() {
-  //   this.api.get(ReferenceDataService.URL)
-  //     .pipe(
-  //       map(responseData => {
-  //         const referenceDataArray = [];
-  //         for (const key in responseData) {
-  //           if (responseData.hasOwnProperty(key)) {
-  //             switch (key) {
-  //               case 'cuisineTypes':
-  //                 this.cuisineTypes.push({...responseData[key]});
-  //                 break;
-  //               case 'foodQualities':
-  //                 this.foodQualities.push({...responseData[key]});
-  //                 break;
-  //               case 'hotelLabels':
-  //                 this.hotelLabels.push({...responseData[key]});
-  //                 break;
-  //               case 'hotelRatings':
-  //                 this.hotelRatings.push({...responseData[key]});
-  //                 console.log('findAll switch');
-  //                 console.log('Hotel ratings:');
-  //                 console.log(this.hotelRatings);
-  //                 console.log('(length:) ');
-  //                 console.log(this.hotelRatings?.length);
-  //                 console.log('[2]: ');
-  //                 console.log(this.hotelRatings[2]);
-  //                 break;
-  //               case 'recommendedTos':
-  //                 this.recommendedTos.push({...responseData[key]});
-  //                 break;
-  //               case 'roomConditions':
-  //                 this.roomConditions.push({...responseData[key]});
-  //                 break;
-  //               case 'roomTypes':
-  //                 this.roomTypes.push({...responseData[key]});
-  //                 break;
-  //               case 'sizes':
-  //                 this.sizes.push({...responseData[key]});
-  //                 break;
-  //             }
-  //             referenceDataArray.push({...responseData[key], id: key});
-  //           }
-  //         }
-  //         return referenceDataArray;
-  //       })
-  //     )
-  //     .subscribe(referenceDataUnits => {
-  //       console.log('referenceDataUnits: ');
-  //       console.log(referenceDataUnits);
-  //     });
+  // public findAll(): Observable<ReferenceData[]> {
+  //   return this.http.get<ReferenceData[]>(this.referenceDataUrl, this.getRequestOptions());
   // }
+  //
+  // private getRequestOptions() {
+  //   const token = sessionStorage.getItem(TOKEN_KEY);
+  //
+  //   const headers: { [key: string]: string } = {
+  //     'Content-Type': 'application/json',
+  //     Authorization: token ? `Bearer ${token}` : ''
+  //   };
+  //
+  //   return {
+  //     headers
+  //   };
+  // }
+
+  private static readonly URL = '/api/reference_data';
+  private cuisines = [];
+  private foodQualities = [];
+  private hotelLabels = [];
+  private hotelRatings = [];
+  private theRecommendedTos = [];
+  private roomConditions = [];
+  private roomTypes = [];
+  private sizes = [];
+
+  constructor(private api: ApiService) {
+  }
+
+  public findAll() {
+    this.api.get(ReferenceDataService.URL)
+      .pipe(
+        map(responseData => {
+          const referenceDataArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              switch (key) {
+                case 'cuisineTypes':
+                  this.cuisines.push(...responseData[key]);
+                  break;
+                case 'foodQualities':
+                  this.foodQualities.push(...responseData[key]);
+                  break;
+                case 'hotelLabels':
+                  this.hotelLabels.push(...responseData[key]);
+                  break;
+                case 'hotelRatings':
+                  this.hotelRatings.push(...responseData[key]);
+                  break;
+                case 'recommendedTos':
+                  this.theRecommendedTos.push(...responseData[key]);
+                  break;
+                case 'roomConditions':
+                  this.roomConditions.push(...responseData[key]);
+                  break;
+                case 'roomTypes':
+                  this.roomTypes.push(...responseData[key]);
+                  break;
+                case 'sizes':
+                  this.sizes.push(...responseData[key]);
+                  break;
+              }
+              referenceDataArray.push({...responseData[key], id: key});
+            }
+          }
+          return referenceDataArray;
+        })
+      )
+      .subscribe(referenceDataUnits => {
+        console.log('referenceDataUnits: ');
+        console.log(referenceDataUnits);
+      });
+  }
   //
   // public getHotelRatings() {
   //   console.log('getHotelRatings');
@@ -109,5 +102,5 @@ export class ReferenceDataService {
   //   console.log(this.hotelRatings[2]);
   //   return this.hotelRatings;
   // }
-  //
+
 }
