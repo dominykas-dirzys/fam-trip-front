c9b00ccfdc6ea9a8a06d9b089222dcda01bab330
Sarunas Janickas <sarunas.janickas@gmail.com>
2021-03-12 23:48:56 +0200

    removed add hotel button from app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import {HotelComponent} from './components/hotel/hotel.component';
 import {RoomComponent} from './components/room/room.component';
 import {HotelListComponent} from './components/hotel-list/hotel-list.component';
 import {RoomListComponent} from './components/room-list/room-list.component';
-import {HotelFormComponent} from './components/hotel-form/hotel-form.component';
-import {RoomEditComponent} from './components/room-edit/room-edit.component';
 
 const routes: Routes = [
   {
@@ -23,10 +21,6 @@ const routes: Routes = [
   {
     path: 'rooms/:id',
     component: RoomComponent
-  },
-  {
-    path: 'addHotel',
-    component: HotelFormComponent
   }
 ];
 
