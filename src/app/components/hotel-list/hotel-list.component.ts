b4f8102367eb1081278360447df89db3bd298f0a
Dominykas Antanas Diržys <dominykasdirzys@gmail.com>
2021-03-13 22:52:11 +0200

    added hotel filter (not working)

--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from '../../services/api.service';
 import {MatDialog} from '@angular/material/dialog';
 import {Hotel} from '../../types/types';
 import {HotelFormComponent} from '../hotel-form/hotel-form.component';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
   styleUrls: ['./hotel-list.component.css']
 })
-export class HotelListComponent implements OnInit {
+export class HotelListComponent implements OnInit, AfterViewInit {
 
   private static readonly URL = '/api/hotels';
 
@@ -17,11 +20,31 @@ export class HotelListComponent implements OnInit {
 
   hotels: Hotel[] = [];
 
+  dataSource: MatTableDataSource<Hotel>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private api: ApiService, public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.load();
+    this.dataSource = new MatTableDataSource(this.hotels);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   delete(id: number) {
